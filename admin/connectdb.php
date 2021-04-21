6052f5ed9ea39851e11a9c126a24283eb8b5acba
fxbite <letruongthinh1703@gmail.com>
2021-04-21 23:42:07 +0700

    PDO connectDB

--- admin/connectdb.php
+++ admin/connectdb.php
@@ -1,5 +1,5 @@
-<?php
-function connectDB() {
+
+<!-- function connectDB() {
     //Get Heroku ClearDB connection information
     $cleardb_server = "us-cdbr-east-03.cleardb.com";
     $cleardb_username = "bf9a74b7b1e568";
@@ -13,6 +13,23 @@ function connectDB() {
         die("Connection failed: " . mysqli_connect_error());
     } 
     return $conn;
-} 
+}  -->
+
+<?php
+function connectDB() {
+    $servername = "us-cdbr-east-03.cleardb.com";
+    $username = "bf9a74b7b1e568";
+    $password = "97595a6d";
+    $myDB = "heroku_19306ce81a4f7f1";
+
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    }
+}
 ?>
 
