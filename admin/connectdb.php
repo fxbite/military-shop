3823ae6069c3ec484844d2714e1e8d9d8a7c606e
fxbite <letruongthinh1703@gmail.com>
2021-04-21 23:31:54 +0700

    connectdb manual

--- admin/connectdb.php
+++ admin/connectdb.php
@@ -1,11 +1,10 @@
 <?php
 function connectDB() {
     //Get Heroku ClearDB connection information
-    $cleardb_url = parse_url(getenv("mysql://bf9a74b7b1e568:97595a6d@us-cdbr-east-03.cleardb.com/heroku_19306ce81a4f7f1?reconnect=true"));
-    $cleardb_server = $cleardb_url["host"];
-    $cleardb_username = $cleardb_url["user"];
-    $cleardb_password = $cleardb_url["pass"];
-    $cleardb_db = substr($cleardb_url["path"],1);
+    $cleardb_server = "us-cdbr-east-03.cleardb.com";
+    $cleardb_username = "bf9a74b7b1e568";
+    $cleardb_password = "97595a6d";
+    $cleardb_db = "heroku_19306ce81a4f7f1";
 
     // Connect to DB
     $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
