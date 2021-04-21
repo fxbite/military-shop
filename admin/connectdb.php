9c2cd3ed3b14323395dbc8806d6d24aaf5c00a34
fxbite <letruongthinh1703@gmail.com>
2021-04-21 21:44:08 +0700

    update database

--- admin/connectdb.php
+++ admin/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 function connectDB() {
     //Get Heroku ClearDB connection information
-    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_url = parse_url(getenv("mysql://bf9a74b7b1e568:97595a6d@us-cdbr-east-03.cleardb.com/heroku_19306ce81a4f7f1?reconnect=true"));
     $cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
     $cleardb_username = $cleardb_url["bf9a74b7b1e568"];
     $cleardb_password = $cleardb_url["97595a6d"];
